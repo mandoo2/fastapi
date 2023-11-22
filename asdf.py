02b789d737d75062e7b6b6eae1a8dc24b2676b03
hotto <hyk6535@gmail.com>
2023-11-22 09:16:14 +0900

    파이보 프로젝트 최초 커밋

---
+++ asdf.py
@@ -0,0 +1,19 @@
+a = 1+1
+
+print(a)
+
+
+
+5 + 4
+_ + 6 
+
+
+a, *_, b = (7, 6, 5, 4, 3, 2, 1)
+print(a, b)
+
+for _ in range(5):
+    print(_)
+
+languages = ["Python", "JS", "PHP", "Java"]
+for _ in languages:
+    print(_)
